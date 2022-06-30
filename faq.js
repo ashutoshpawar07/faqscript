e13c82b04c97fb213783ff17fc50df40f836f366
ashutoshpawar07 <ashutoshpawar07@gmail.com>
2022-06-30 11:53:42 +0530

    added onload

--- faq.js
+++ faq.js
@@ -1,5 +1,9 @@
+window.addEventListener("load",()=>{
 const button = document.createElement('FAQ Button')
 button.innerHTML = "FAQ button click here"
 const section = document.getElementById('myfaqsection')
 section.appendChild(button)
 //comment
+
+	
+})
